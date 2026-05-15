43dae403b4b42ff65c40326173ae1a3a441b0629
T3 Code <t3code@users.noreply.github.com>
2026-05-14 17:20:04 -0700

    t3 checkpoint ref=refs/t3/checkpoints/YTUyYWZhMzYtNDY2My00NzVkLThjOGYtMTQ2Mjc1MDJkZDdj/turn/26

---
+++ lyra-server/src/plugins/metadata/ensure.rs
@@ -0,0 +1,22 @@
+// This Source Code Form is subject to the terms of the Lyra Public License,
+// v1.0. If a copy of the Lyra Public License was not distributed with this file,
+// You can obtain one here:
+// www.meshiplaw.com/lyra.
+
+#[cfg(feature = "docgen")]
+use harmony_luau::DescribeInterface;
+
+#[harmony_macros::interface]
+pub(super) struct EnsureArtistRequest {
+    id_type: String,
+    id_value: String,
+    artist_name: Option<String>,
+    sort_name: Option<String>,
+    artist_type: Option<String>,
+    description: Option<String>,
+}
+
+#[cfg(feature = "docgen")]
+pub(super) fn interface_descriptors() -> Vec<harmony_luau::InterfaceDescriptor> {
+    vec![EnsureArtistRequest::interface_descriptor()]
+}
