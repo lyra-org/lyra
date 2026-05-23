968bf658cb909f931615d7214476870dfdef1d33
T3 Code <t3code@users.noreply.github.com>
2026-05-23 14:38:18 -0700

    t3 checkpoint ref=refs/t3/checkpoints/MmM4MWM4YTEtMTVhNy00OWU4LWFkZDItM2Y0NGMyMGY3YjQ2/turn/1

---
+++ lyra-server/src/services/remote/mod.rs
@@ -0,0 +1,9 @@
+// This Source Code Form is subject to the terms of the Lyra Public License,
+// v1.0. If a copy of the Lyra Public License was not distributed with this file,
+// You can obtain one here:
+// www.meshiplaw.com/lyra.
+
+pub(crate) mod connection;
+pub(crate) mod constants;
+pub(crate) mod messages;
+pub(crate) mod registry;
