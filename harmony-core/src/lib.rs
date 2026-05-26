018acecb7b7a36375dcaebf9053138fc662b70be
T3 Code <t3code@users.noreply.github.com>
2026-05-26 15:13:32 -0700

    t3 checkpoint ref=refs/t3/checkpoints/N2IyMWJkNjMtNDc2MS00N2VkLWIwYzgtZTVjMTEyYWUzMGE4/turn/15

---
+++ harmony-core/src/lib.rs
@@ -0,0 +1,72 @@
+// This Source Code Form is subject to the terms of the Lyra Public License,
+// v1.0. If a copy of the Lyra Public License was not distributed with this file,
+// You can obtain one here:
+// www.meshiplaw.com/lyra.
+
+mod luaurc;
+pub mod luau;
+mod modules;
+pub mod plugin;
+mod scheduler;
+mod tokio_runtime;
+mod userdata;
+
+pub use luaurc::LuaurcConfig;
+pub use modules::{
+    AllowAllCapabilities,
+    CapabilityPolicy,
+    FilesystemSourceLoader,
+    FunctionSpec,
+    GlobalSpec,
+    MemorySourceLoader,
+    ModuleCacheKey,
+    ModuleCapabilityDenied,
+    ModuleExport,
+    ModuleLoadContext,
+    ModuleLoadError,
+    ModuleRegistry,
+    ModuleSpec,
+    NativeModuleInstaller,
+    ResolvedSource,
+    SourceLoader,
+    SourceRequest,
+    UserDataSpec,
+};
+pub use modules::{
+    FunctionCallback,
+    LuauRequireRuntime,
+    LuauSourceCache,
+    async_luau_callback,
+    install_luau_globals,
+    install_luau_module,
+    install_luau_require,
+};
+pub use plugin::{
+    LoadedPlugin,
+    ManifestCapabilityPolicy,
+    PluginLoadError,
+    PluginManager,
+    PluginManifest,
+    Runtime,
+    RuntimeBuilder,
+};
+pub use scheduler::LocalScheduler;
+pub use scheduler::{
+    CallContext,
+    CapabilityId,
+    ChunkOrigin,
+    ContextBag,
+    ModuleId,
+    ScheduledFuture,
+    Scheduler,
+    TaskGroupId,
+    TaskHandle,
+    TaskId,
+    TaskSnapshot,
+    TaskState,
+};
+pub use tokio_runtime::TokioRuntimeContext;
+pub use userdata::{
+    UserDataClass,
+    UserDataType,
+};
